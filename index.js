18e1de70b7a1cd9366a043ea73757407464b51fb
shiran44 <shirant442@gmail.com>
2017-04-27 13:39:08 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var jumpRope= require('./jumpRope');
-
-var http = require ('http');
-http.createServer(function(req , res){
-    res.writeHeader(200);
-    res.write('success\n');
-    res.end(JSON.stringify(jumpRope.array));
-}).listen(8080);
-
